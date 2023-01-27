9d23ef53f9957caf979e3ad73deaec0c56f8a3d5
ArunRaj2312 <arunvijay1043534@gmail.com>
2023-01-26 16:09:51 -1200

    add file

---
+++ main.js
@@ -0,0 +1,13 @@
+var h=parseInt(prompt("enter  your height"));
+if(h<150){
+    console.log("short");
+}
+else if(h>150 && h<165){
+    console.log("average");
+}
+else if(h>165 && h<195){
+    console.log("tall")
+}
+else{
+    console.log("very tall")
+}
